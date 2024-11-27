f5abe69c77624384cb68afcd0b388f3334269824
lipooliveira <canalassasinogamer@gmail.com>
2024-11-26 21:42:23 -0300

    search add

--- Controllers/MasterController.php
+++ Controllers/MasterController.php
@@ -2,7 +2,47 @@
 class MasterController extends Controller{
 
     public function index(){
-        $this->carregarEstrutura('MasterView');
+        $auth = new AuthController();
+        if(!$auth->isMaster())
+        {
+            $this->carregarEstrutura('ErroView');
+            exit;
+        }
+        $this->listarTodos();
+    }
+
+    public function listarTodos()
+    {
+        $daoUsuario = new UsuarioDAO();
+        $dados = $daoUsuario->listar();
+
+        $dadosArray = array_map(function($obj) {
+            return $obj->toArray();
+        }, $dados);
+        $dadosJson = json_encode($dadosArray);
+
+        $this->carregarEstrutura('MasterView', $dadosJson);        
+
+    }
+
+    public function pesquisar()
+    {
+        $auth = new AuthController();
+        if(!$auth->isMaster())
+        {
+            $this->carregarEstrutura('ErroView');
+            exit;
+        }
+        $query = isset($_POST['query']) ? $_POST['query'] : '';
+        $daoUsuario = new UsuarioDAO();
+        $dados = $daoUsuario->buscar($query);
+
+        $dadosArray = array_map(function($obj) {
+            return $obj->toArray();
+        }, $dados);
+        $dadosJson = json_encode($dadosArray);
+
+        $this->carregarEstrutura('MasterView', $dadosJson);        
     }
 }
 
--- Views/MasterView.php
+++ Views/MasterView.php
@@ -1,3 +1,61 @@
 <section>
-    <p style="text-align: center;">Você não tem permissão para acessar esta página.</p>
+    <div class="container mt-3">
+        <h1>Controle de usuários</h1>
+        <form class="form-inline" action="<?php echo BASE_URL?>/Master/pesquisar" method="POST">
+            <label for="search">Pesquisar usuário:</label>
+            <input class="form-control mr-sm-2" type="search" placeholder="Nome, CPF, Email, ID..." aria-label="Pesquisar" name="query" id="query">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" style="background-color: #f8f9fa;">Pesquisar</button>
+        </form>
+    </div>
+
+
+    <script>
+    document.getElementById('query').addEventListener('input', function (e) {
+        let value = e.target.value;
+        if (value.length === 11 && !isNaN(value)) {
+            value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+            e.target.value = value;
+        }
+    });
+    </script>
+
+    <div class="container mt-3">
+        <h2>Usuários</h2>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">CPF</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $dados = json_decode($dados, true);
+
+                    $usuarios = [];
+                    foreach ($dados as $dado) {
+                        $usuario = new UsuarioModel();
+                        $usuario->setId($dado['id']);
+                        $usuario->setNome($dado['nome']);
+                        $usuario->setCpf($dado['cpf']);
+                        $usuario->setEmail($dado['email']);
+                        $usuarios[] = $usuario;
+                    }
+                    $dados = $usuarios;
+                    foreach($dados as $usuario)
+                    {
+                        echo '<tr>';
+                        echo '<td>'.$usuario->getId().'</td>';
+                        echo '<td>'.$usuario->getNome().'</td>';
+                        echo '<td>'.$usuario->getCpf().'</td>';
+                        echo '<td>'.$usuario->getEmail().'</td>';
+                        echo '<td><a href="'.BASE_URL.'/Master/editar/'.$usuario->getId().'">Editar</a> | <a href="'.BASE_URL.'/Master/excluir/'.$usuario->getId().'">Excluir</a></td>';
+                        echo '</tr>';
+                    }
+                ?>
+            </tbody>
+        </table>
 </section>
