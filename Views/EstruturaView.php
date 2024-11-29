5e6e8b2789caee52a767e395ce5bf3eead12c51e
lipooliveira <canalassasinogamer@gmail.com>
2024-11-28 22:24:36 -0300

    clear project

--- Views/EstruturaView.php
+++ Views/EstruturaView.php
@@ -140,18 +140,6 @@
     <!-- Preloader -->
     <div id="preloader"></div>
 
-    <!-- Vendor JS Files -->
-    <script src="<?php echo BASE_URL; ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="<?php echo BASE_URL; ?>/assets/vendor/php-email-form/validate.js"></script>
-    <script src="<?php echo BASE_URL; ?>/assets/vendor/aos/aos.js"></script>
-    <script src="<?php echo BASE_URL; ?>/assets/vendor/typed.js/typed.umd.js"></script>
-    <script src="<?php echo BASE_URL; ?>/assets/vendor/purecounter/purecounter_vanilla.js"></script>
-    <script src="<?php echo BASE_URL; ?>/assets/vendor/waypoints/noframework.waypoints.js"></script>
-    <script src="<?php echo BASE_URL; ?>/assets/vendor/glightbox/js/glightbox.min.js"></script>
-    <script src="<?php echo BASE_URL; ?>/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-    <script src="<?php echo BASE_URL; ?>/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-    <script src="<?php echo BASE_URL; ?>/assets/vendor/swiper/swiper-bundle.min.js"></script>
-
     <!-- Main JS File -->
     <script src="<?php echo BASE_URL; ?>/assets/js/main.js"></script>
 
