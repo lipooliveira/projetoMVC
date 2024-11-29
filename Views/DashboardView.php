cbf1eeef7858ab6789d9680801b7e44e0cd0bfbc
lipooliveira <canalassasinogamer@gmail.com>
2024-11-28 22:56:57 -0300

    otimizacao final

--- Views/DashboardView.php
+++ Views/DashboardView.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-4">
                 <div class="card">
-                    <img src="<?php echo BASE_URL; ?>/assets/img/products/iphone.png" class="card-img-top" alt="Product 1">
+                    <img src="<?php echo BASE_URL; ?>/assets/img/products/iphone.jpg" class="card-img-top" alt="Product 1" style="max-height: 50vh;">
                     <div class="card-body">
                         <h2 class="card-title">Iphone 15 Pro MAX</h2>
                         <h3 class="card-text">R$8799.99</h3>
@@ -12,7 +12,7 @@
             </div>
             <div class="col-md-4">
                 <div class="card">
-                    <img src="<?php echo BASE_URL; ?>/assets/img/products/samsung.png" class="card-img-top" alt="Product 2">
+                    <img src="<?php echo BASE_URL; ?>/assets/img/products/samsung.jpg" class="card-img-top" alt="Product 2" style="max-height: 50vh;">
                     <div class="card-body">
                         <h2 class="card-title">Samsung S24 Ultra</h2>
                         <h3 class="card-text">R$8999.99</h3>
@@ -21,7 +21,7 @@
             </div>
             <div class="col-md-4">
                 <div class="card">
-                    <img src="<?php echo BASE_URL; ?>/assets/img/products/motorola.webp" class="card-img-top" alt="Product 3">
+                    <img src="<?php echo BASE_URL; ?>/assets/img/products/motorola.webp" class="card-img-top" alt="Product 3" style="max-height: 50vh;">
                     <div class="card-body">
                         <h2 class="card-title">Motorola Edge 50 Ultra</h2>
                         <h3 class="card-text">R$4999.99</h3>
